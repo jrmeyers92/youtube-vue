9863cb6ccbc1e95e903f3bb37da79ed8630bef05
Jake Meyers <JakeRMeyers92@gmail.com>
2021-11-17 15:35:10 -0600

    init

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: (e) => {
+      console.log(e.target.value);
+    },
+  },
+};
+</script>
+
+<style></style>
